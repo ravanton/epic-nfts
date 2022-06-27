60728fdda6ff363bdf10260351481267ec49eacd
ravanton <antonrekhva@gmail.com>
2022-06-27 15:46:23 +0300

    First commit

---
+++ contracts/MyEpicNFT.sol
@@ -0,0 +1,105 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.1;
+
+
+import "@openzeppelin/contracts/utils/Strings.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "hardhat/console.sol";
+
+// We need to import the helper functions from the contract that we copy/pasted.
+import { Base64 } from "./libraries/Base64.sol";
+
+contract MyEpicNFT is ERC721URIStorage {
+  using Counters for Counters.Counter;
+  Counters.Counter private _tokenIds;
+
+  string baseSvg = "<svg xmlns='http://www.w3.org/2000/svg' preserveAspectRatio='xMinYMin meet' viewBox='0 0 350 350'><style>.base { fill: black; font-family: satoshi; font-size: 34px; }</style><rect width='100%' height='100%' fill='orange' /><text x='50%' y='50%' class='base' dominant-baseline='middle' text-anchor='middle'>";
+  
+  string[] firstWords = ["Take", "You", "What", "Kiss", "Love", "Where", "They", "My", "I", "We"];
+  string[] secondWords = ["Pussy", "Fool", "Whore", "Crap", "Ass", "Pussy", "Motherfucker", "Bullshit", "Pussy"];
+  string[] thirdWords = ["Fucking", "Shit", "Get", "Yep", "Go", "Just", "Lost", "Innit", "Prick", "Sweet"];
+  
+  event NewEpicNFTMinted(address sender, uint256 tokenId);
+
+  constructor() ERC721 ("SquareNFT", "SQUARE") {
+    console.log("This is my NFT contract. Woah!");
+  }
+
+  function pickRandomFirstWord(uint256 tokenId) public view returns (string memory) {
+    uint256 rand = random(string(abi.encodePacked("FIRST_WORD", Strings.toString(tokenId))));
+    rand = rand % firstWords.length;
+    return firstWords[rand];
+  }
+
+  function pickRandomSecondWord(uint256 tokenId) public view returns (string memory) {
+    uint256 rand = random(string(abi.encodePacked("SECOND_WORD", Strings.toString(tokenId))));
+    rand = rand % secondWords.length;
+    return secondWords[rand];
+  }
+
+  function pickRandomThirdWord(uint256 tokenId) public view returns (string memory) {
+    uint256 rand = random(string(abi.encodePacked("THIRD_WORD", Strings.toString(tokenId))));
+    rand = rand % thirdWords.length;
+    return thirdWords[rand];
+  }
+
+  function random(string memory input) internal pure returns (uint256) {
+      return uint256(keccak256(abi.encodePacked(input)));
+  }
+
+  function makeAnEpicNFT() public {
+    uint256 newItemId = _tokenIds.current();
+
+    string memory first = pickRandomFirstWord(newItemId);
+    string memory second = pickRandomSecondWord(newItemId);
+    string memory third = pickRandomThirdWord(newItemId);
+    string memory combinedWord = string(abi.encodePacked(first, second, third));
+
+    string memory finalSvg = string(abi.encodePacked(baseSvg, combinedWord, "</text></svg>"));
+    
+    // Get all the JSON metadata in place and base64 encode it.
+    string memory json = Base64.encode(
+        bytes(
+            string(
+                abi.encodePacked(
+                    '{"name": "',
+                    // We set the title of our NFT as the generated word.
+                    combinedWord,
+                    '", "description": "SAY IT", "image": "data:image/svg+xml;base64,',
+                    // We add data:image/svg+xml;base64 and then append our base64 encode our svg.
+                    Base64.encode(bytes(finalSvg)),
+                    '"}'
+                )
+            )
+        )
+    );
+
+    // Just like before, we prepend data:application/json;base64, to our data.
+    string memory finalTokenUri = string(
+        abi.encodePacked("data:application/json;base64,", json)
+    );
+
+    console.log("\n--------------------");
+    console.log(
+        string(
+            abi.encodePacked(
+              "https://nftpreview.0xdev.codes/?code=",
+              finalTokenUri
+            )
+        )
+    );
+    console.log("--------------------\n");
+
+    _safeMint(msg.sender, newItemId);
+    
+    // Update your URI!!!
+    _setTokenURI(newItemId, "ipfs://QmPuA1TqhyEmJdSKSsob8yznCax3GzDaoKrEziy4uuwMmK");
+  
+    _tokenIds.increment();
+    console.log("An NFT w/ ID %s has been minted to %s", newItemId, msg.sender);
+
+    emit NewEpicNFTMinted(msg.sender, newItemId);
+  }
+}
